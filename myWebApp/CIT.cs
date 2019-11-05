accac52db834d73c3c0b3b94c533dd049fee4b36
kalbrechtova <56029905+kalbrechtova@users.noreply.github.com>
2019-11-05 13:39:23 +0100

    Add files via upload

---
+++ myWebApp/CIT.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myWebApp
+{
+    public class CIT
+    {
+        public Boolean isValid() {
+            List<int> numbers = new List<int>();
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+
+
+            return true;
+
+        }
+       
+        
+    }
+}
---
+++ myWebApp/numbers/Add.cs
@@ -0,0 +1,6 @@
+﻿namespace numbers
+{
+    internal class Add
+    {
+    }
+}
